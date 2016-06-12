96b6df817f592fe362fd79872ff225ccc633d996
Joe Hermaszewski <git@monoid.al>
2016-06-12 14:52:03 +0100

    Use pulls

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
 { nixpkgs
 , declInput 
+, githubPulls
 }:
 
 let
@@ -8,7 +9,7 @@ let
   teethBranch = branch: {
     enabled = 1;
     hidden = false;
-    description = "teeth ${branch}";
+    description = "teeth ${branch} " + githubPulls;
     nixexprinput = "src";
     nixexprpath = "release.nix";
     checkinterval = 60;
